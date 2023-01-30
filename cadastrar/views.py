1abdb49c876275c71b99ac686b85055f057da288
icaromoroni <icaromorone@gmail.com>
2023-01-30 10:18:09 -0300

    adicionando formulario de cadastro e aplicação widget

--- cadastrar/views.py
+++ cadastrar/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-from cadastrar.forms import CadastroForms
+from cadastrar.forms import CadastroForms,Itens
 
 def home(request):
     return render(request, 'formularios/home.html')
 
 def index(request):
     form = CadastroForms()
-    contexto = {'form': form}
+    itensForm = Itens()
+    contexto = {'form': form, 'itensForm': itensForm}
     return render(request, 'formularios/index.html', contexto)
--- teste/settings.py
+++ teste/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'cadastrar',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
