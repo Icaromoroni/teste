d5144caa39c5c1760aaee1feeae4f9c2f1951611
icaromoroni <icaromorone@gmail.com>
2023-01-31 16:17:07 -0300

    alteracoes nos formularios do app cadastrar

--- cadastrar/forms.py
+++ cadastrar/forms.py
@@ -1,13 +1,31 @@
 from django import forms
 
+from .models import Cadastros, Inventario
+
 SEXO = (('M','Masculino'), ('F', 'Feminino'))
 
-class CadastroForms(forms.Form):
+'''class CadastroForms(forms.Form):
     nome = forms.CharField(label='nome',max_length=100)
     idade = forms.IntegerField(label='idade')
     sexo = forms.ChoiceField(label='sexo',choices=SEXO)
     latitude = forms.IntegerField(label='latitude')
-    longitude = forms.IntegerField(label='latitude')
+    longitude = forms.IntegerField(label='latitude')'''
 
-class Itens(forms.Form):
-    pass
+'''class Inventario(forms.Form):
+    pass'''
+
+class CadastroForms(forms.ModelForm):
+    
+    class Meta:
+
+        model = Cadastros
+
+        fields = ['nome', 'idade', 'sexo', 'latitude', 'longitude']
+
+class InventarioForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Inventario
+
+        fields = '__all__'
--- cadastrar/views.py
+++ cadastrar/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from cadastrar.forms import CadastroForms
 
 from cadastrar.models import Cadastros
+from django.contrib import messages
 
 def home(request):
     return render(request, 'formularios/home.html')
@@ -11,11 +12,23 @@ def listaCadastro(request):
     cadastro = Cadastros.objects.all()
     return render(request, 'formularios/lista.html')
 
+def index(request):
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Cadastro realizado com sucesso.')
+            return redirect("/")
+
+    else:
+        form = CadastroForms()
+        contexto = {'form': form}
+        return render(request, 'formularios/index.html', contexto)
+
+
 def sobreviventeViews(request, id):
     sobreviventeViews = get_object_or_404(Cadastros, pk=id)
     return render(request, 'formularios/sobreviventeViews.html', {'sobreviventeViews': sobreviventeViews})
 
-def index(request):
-    form = CadastroForms()
-    contexto = {'form': form}
-    return render(request, 'formularios/index.html', contexto)
+
+    
